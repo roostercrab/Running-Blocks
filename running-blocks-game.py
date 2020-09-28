35719aba878f0c5059b6c1f88ab44bd5238b9236
Jesse Shea <jesseshea498@gmail.com>
2020-09-28 15:19:29 -0700

    made changes

--- running-blocks-game.py
+++ running-blocks-game.py
@@ -1,20 +1,17 @@
 import pygame
-import random
 import sys
-import speed
-import collision
+ 
 
 pygame.init()
 clock = pygame.time.Clock()
-myFont = pygame.font.SysFont("monospace", 35)
 
+# INITIALIZE SCREEN AND COLORS
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 800
-
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BACKGROUND_COLOR = (0,0,0)
-ENEMY_SPEED = 10
 
 #INITIALIZE SCORE
 score = 0
@@ -26,7 +23,6 @@ player_x_coord = player_pos[0]
 player_y_coord = player_pos[1]
 player_color = BLUE
 
-
 # INITIALIZE ENEMY
 enemy_size = 50
 enemy_pos = [750, 525]
@@ -34,9 +30,6 @@ enemy_x_coord = enemy_pos[0]
 enemy_y_coord = enemy_pos[1]
 enemy_color = GREEN
 enemy_list = [enemy_pos]
-
-# INITIALIZE SCREEN
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # GAME LOOP
 game_over = False
@@ -46,40 +39,26 @@ while not game_over:
     if event.type == pygame.QUIT:
       sys.exit()
 
-  # PLAYER BLOCK MOVEMENT
+  # PLAYER JUMP
     if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_UP:
-        player_y_coord -= player_size * 2
-      # elif event.key == pygame.K_DOWN:
-      #   player_size = player_size * .5
+      if event.key == pygame.K_SPACE:
+        hangtime = 1
+        while hangtime <= 50:
+          player_y_coord -= hangtime
+          hangtime = hangtime + hangtime
+        player_pos[1] = player_y_coord
+        pygame.draw.rect(screen, player_color, (player_x_coord, player_y_coord, player_size, player_size))
 
-      player_pos[1] = player_y_coord
-    
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_UP:
-        player_y_coord += player_size * 2
-      # elif event.key == pygame.K_DOWN:
-      #   player_size = player_size * 2
+        hangtime = 50
+        while hangtime >= 1:
+          player_y_coord += hangtime
+          hangtime = hangtime - hangtime
+        player_pos[1] = player_y_coord
+        pygame.draw.rect(screen, player_color, (player_x_coord, player_y_coord, player_size, player_size))
 
-      player_pos[1] = player_y_coord
 
   # PAINT BACKGROUND
   screen.fill(BACKGROUND_COLOR)
-
-  # ENEMY IMAGE DROP and DRAW
-#   drop_enemies(enemy_list)
-
-#   text = "Score: " + str(score)
-#   label = myFont.render(text, 1, (255,255,0))
-#   screen.blit(label, (SCREEN_WIDTH-200, SCREEN_HEIGHT-40))
-
-#   score = update_enemy_positions(enemy_list, score)
-#   print(score)
-
-#   if collision.collision_check(enemy_list, player_pos, player_size, enemy_size):
-#     game_over = True
-
-#   draw_enemies(enemy_list)
 
   # PLAYER IMAGE DRAW
   pygame.draw.rect(screen, player_color, (player_x_coord, player_y_coord, player_size, player_size))
