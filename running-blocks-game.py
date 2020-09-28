209e7e7083d1ce77a5f134751fae04d9e94faa85
Jesse Shea <jesseshea498@gmail.com>
2020-09-28 14:27:24 -0700

    blue a nd green in position

--- running-blocks-game.py
+++ running-blocks-game.py
@@ -4,101 +4,88 @@ import sys
 import speed
 import collision
 
-
 pygame.init()
 clock = pygame.time.Clock()
 myFont = pygame.font.SysFont("monospace", 35)
 
-SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
 
-RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BACKGROUND_COLOR = (0,0,0)
 ENEMY_SPEED = 10
 
-# INITIALLY DEFINE PLAYER
-player_size = 50
-player_pos = [SCREEN_WIDTH + player_size, SCREEN_HEIGHT - player_size]
-player_x_coord = player_pos[0]
-player_y_coord = player_pos[1]
+#INITIALIZE SCORE
 score = 0
 
-# INITIALLY DEFINE ENEMY
+# INITIALIZE PLAYER
+player_size = 50
+player_pos = [50, 525]
+player_x_coord = player_pos[0]
+player_y_coord = player_pos[1]
+player_color = BLUE
+
+
+# INITIALIZE ENEMY
 enemy_size = 50
-enemy_pos = [random.randint(0, SCREEN_WIDTH - enemy_size), 0]
+enemy_pos = [750, 525]
 enemy_x_coord = enemy_pos[0]
 enemy_y_coord = enemy_pos[1]
+enemy_color = GREEN
 enemy_list = [enemy_pos]
 
-# DEFINE SCREEN
+# INITIALIZE SCREEN
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# FUNCTIONS
-def drop_enemies(enemy_list):
-  delay = random.random()
-  if len(enemy_list) < 10 and delay < 0.1:
-    x_pos = random.randint(0, SCREEN_WIDTH - enemy_size)
-    y_pos = 0
-    enemy_list.append([x_pos, y_pos])
-
-def draw_enemies(enemy_list):
-  for enemy_pos in enemy_list:
-      enemy_x_coord = enemy_pos[0]
-      enemy_y_coord = enemy_pos[1]
-      pygame.draw.rect(screen, BLUE, (enemy_x_coord, enemy_y_coord, enemy_size, enemy_size))
-
-def update_enemy_positions(enemy_list, score):
-  for idx, enemy_pos in enumerate(enemy_list):
-    enemy_y_coord = enemy_pos[1]
-    enemy_y_coord = int(enemy_y_coord)
-    if enemy_y_coord >= 0 and enemy_y_coord < SCREEN_HEIGHT:
-      enemy_pos[1] += ENEMY_SPEED
-    else:
-      enemy_list.pop(idx)
-      score += 1
-  return score
-
 
 # GAME LOOP
 game_over = False
 while not game_over:
   for event in pygame.event.get():
-    # print(event)
+    print(event)
     if event.type == pygame.QUIT:
       sys.exit()
 
   # PLAYER BLOCK MOVEMENT
     if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_UP:
+        player_y_coord -= player_size * 2
+      # elif event.key == pygame.K_DOWN:
+      #   player_size = player_size * .5
 
-      if event.key == pygame.K_LEFT:
-        player_x_coord -= (player_size * .5)
+      player_pos[1] = player_y_coord
+    
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_UP:
+        player_y_coord += player_size * 2
+      # elif event.key == pygame.K_DOWN:
+      #   player_size = player_size * 2
 
-      elif event.key == pygame.K_RIGHT:
-        player_x_coord += (player_size * .5)
-
-      player_pos[0] = player_x_coord
+      player_pos[1] = player_y_coord
 
   # PAINT BACKGROUND
   screen.fill(BACKGROUND_COLOR)
 
   # ENEMY IMAGE DROP and DRAW
-  drop_enemies(enemy_list)
+#   drop_enemies(enemy_list)
 
-  text = "Score: " + str(score)
-  label = myFont.render(text, 1, (255,255,0))
-  screen.blit(label, (SCREEN_WIDTH-200, SCREEN_HEIGHT-40))
+#   text = "Score: " + str(score)
+#   label = myFont.render(text, 1, (255,255,0))
+#   screen.blit(label, (SCREEN_WIDTH-200, SCREEN_HEIGHT-40))
 
-  score = update_enemy_positions(enemy_list, score)
-  print(score)
+#   score = update_enemy_positions(enemy_list, score)
+#   print(score)
 
-  if collision.collision_check(enemy_list, player_pos, player_size, enemy_size):
-    game_over = True
+#   if collision.collision_check(enemy_list, player_pos, player_size, enemy_size):
+#     game_over = True
 
-  draw_enemies(enemy_list)
+#   draw_enemies(enemy_list)
 
   # PLAYER IMAGE DRAW
-  pygame.draw.rect(screen, RED, (player_x_coord, player_y_coord, player_size, player_size))
+  pygame.draw.rect(screen, player_color, (player_x_coord, player_y_coord, player_size, player_size))
+
+  # ENEMY IMAGE DRAW
+  pygame.draw.rect(screen, enemy_color, (enemy_x_coord, enemy_y_coord, enemy_size, enemy_size))
 
   # UPDATE SCREEN
   clock.tick(30)
