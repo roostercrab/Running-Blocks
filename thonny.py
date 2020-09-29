4e7f78d00827ac3e9b64317bae50e423e60a8900
roostercrab <jesse.shea@qixingdesign.com>
2020-09-29 14:55:54 -0700

    figured out jumping

--- thonny.py
+++ thonny.py
@@ -1,9 +1,3 @@
-import pygame
-
-pygame.init()
-pygame.display.set_caption("Running Blocks")
-window = pygame.display.set_mode((500, 500))
-
 player_x = 50
 player_y = 400
 player_width = 40
@@ -27,30 +21,18 @@ def jump(player_y, jump_velocity):
 
 run = True
 while run:
-  window.fill((0,0,0))
-  pygame.draw.rect(window, (0,0,255), (player_x, player_y, player_width, player_height))
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
 
   if jump_progress == []:
-    player_y = 400
-    jump_velocity = 10
     jumping = False
   else:
     player_y = jump_progress[0]
     jump_velocity = jump_progress[1]
 
   # PLAYER BLOCK MOVEMENT
-  keypress = pygame.key.get_pressed()
+  keypress = True
 
-  if keypress[pygame.K_SPACE]:
+  if keypress == True:
     jumping = True
 
   if jumping == True:
       jump_progress = jump(player_y, jump_velocity)
-
-  pygame.time.delay(10)
-  pygame.display.update()
-
-pygame.quit()
