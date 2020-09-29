e326428952f1957df377764b8f11bfef0457fb31
roostercrab <jesse.shea@qixingdesign.com>
2020-09-29 13:13:40 -0700

    damn thing...

--- running-blocks-game.py
+++ running-blocks-game.py
@@ -8,15 +8,15 @@ player_x = 50
 player_y = 400
 player_width = 40
 player_height = 60
-velocity = 5
+
 
 jumping = False
-hangtime = 10
+jump_velocity = 10
 
 run = True
 while run:
-
-
+  window.fill((0,0,0))
+  pygame.draw.rect(window, (0,0,255), (player_x, player_y, player_width, player_height))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
@@ -24,24 +24,18 @@ while run:
     # PLAYER BLOCK MOVEMENT
     keypress = pygame.key.get_pressed()
 
-    if not(jumping):
-      if keypress[pygame.K_SPACE]:
-        jumping = True
-    else:
-      if hangtime >= -10:
-        gravity = 1
-        if hangtime < 0:
-          gravity = -1
-        player_y -= int((hangtime ** 2) * 0.5 * gravity)
-        hangtime -= 1
-      else:
+    if jumping is False and keypress[pygame.K_SPACE]:
+      jumping = True
+
+    if jumping is True:
+      player_y -= jump_velocity
+      jump_velocity -= 1
+      if jump_velocity < -10:
         jumping = False
-        hangtime = 10
+        jump_velocity = 10
 
 
-  window.fill((0,0,0))
-  pygame.draw.rect(window, (0,0,255), (player_x, player_y, player_width, player_height))
-  pygame.time.delay(30)
+  pygame.time.delay(1)
   pygame.display.update()
 
 pygame.quit()
