5af6e22bf71112217771aad1c8fed90df9b6c95d
roostercrab <jesse.shea@qixingdesign.com>
2020-09-30 15:21:05 -0700

    collisions detected, life meter added, score added

--- collision.py
+++ collision.py
@@ -1,40 +1,22 @@
-def collision_check(tree_list, player_pos, player_size, tree_size):
-  player_x = player_pos[0]
-  player_y = player_pos[1]
-  player_x = int(player_x)
-  player_y = int(player_y)
-  for tree_pos in tree_list:
-    tree_x = tree_pos[0]
-    tree_y = tree_pos[1]
-    tree_x = int(tree_x)
-    tree_y = int(tree_y)
-
-    if detect_collision(player_x, player_y, tree_x, tree_y, player_size, tree_size):
-      return True
-  return False
-
-def detect_collision(player_x, player_y, tree_x, tree_y, player_size, tree_size):
-  player_x = int(player_x)
-  player_y = int(player_y)
-  tree_x = int(tree_x)
-  tree_y = int(tree_y)
-
+def check(player_x, player_y, player_height, player_width, tree_list, tree_height, tree_width):
   player_x_space = range(player_x, player_x + player_width)
   player_y_space = range(player_y, player_y + player_height)
-
-  tree_x_space = range(tree_x, tree_x + tree_size)
-  tree_y_space = range(tree_y, tree_y + tree_size)
-
   player_x_set = set(player_x_space)
-  tree_x_set = set(tree_x_space)
-  x_set_result = player_x_set.intersection(tree_x_set)
 
-  player_y_set = set(player_y_space)
-  tree_y_set = set(tree_y_space)
-  y_set_result = player_y_set.intersection(tree_y_set)
+  for tree in tree_list:
+    tree_x = int(tree[0])
+    tree_y = int(tree[1])
 
-  if x_set_result and y_set_result != set():
-    print("GAME OVER")
-    return True
-  else:
+    tree_x_space = range(tree_x, tree_x + tree_width)
+    tree_y_space = range(tree_y, tree_y + tree_height)
+    tree_x_set = set(tree_x_space)
+
+    x_set_result = player_x_set.intersection(tree_x_set)
+
+    player_y_set = set(player_y_space)
+    tree_y_set = set(tree_y_space)
+    y_set_result = player_y_set.intersection(tree_y_set)
+
+    if x_set_result and y_set_result != set():
+      return True
     return False
---
+++ jump.py
@@ -0,0 +1,10 @@
+def up(player_y, jump_velocity):
+    if jump_velocity < -20:
+      jump_velocity = 20
+      jump_progress = []
+      return jump_progress
+    else:
+      player_y -= jump_velocity
+      jump_velocity -= 1
+      jump_progress = [player_y, jump_velocity]
+      return jump_progress
